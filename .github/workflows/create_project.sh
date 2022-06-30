bea96c8ba0659662c9a1e318a6c712abb1f4d093
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-06-29 20:46:35 -0500

    action

--- .github/workflows/create_project.sh
+++ .github/workflows/create_project.sh
@@ -5,9 +5,6 @@ full_name=$1
 echo $full_name
 
 repo=$(echo $full_name | awk -F '/' '{print $2}')
-author=$(echo $full_name | awk -F '/' '{print $1}')
-owner=$(echo $full_name | awk -F '/' '{print $1}')
-package=$(echo $full_name | awk -F '/' '{print $2}' | tr '-' '_' | tr '[:upper:]' '[:lower:]')
 
 original_repo="gladier-client-template"
 
@@ -19,7 +16,5 @@ mv README.md HOW-TO.md
 cp .github/README_STD.md README.md
 sed -i "s/$original_repo/$repo/g" README.md
 
-cp -r full_client $name
-mv $name/full_client.py $name/$name.py
-rm -rf .github
-rm -rf CITATION.cff
+cp -r full_client $repo
+mv $repo/full_client.py $repo/$repo\_client.py
